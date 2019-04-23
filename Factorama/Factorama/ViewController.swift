109874d354eb514e12d8b702ee0af4454ea3803d
Heidi <heidi.smith@students.svu.edu>
2019-04-23 15:03:11 -0400

    Added in score stuff

--- Factorama/Factorama/ViewController.swift
+++ Factorama/Factorama/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     @IBOutlet var NumberToFactorLabelOutlet: UILabel!
     @IBOutlet var StartButtonOutet: UIButton!
     
+    @IBOutlet var totalScoreLabel: UILabel!
     // var factorGame = FactorGame()
     
     override func viewDidLoad() {
@@ -24,6 +25,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         PickerOutlet.dataSource = self
         
         PickerOutlet.selectRow(0, inComponent: 0, animated: false)
+        totalScoreLabel.text! = "Score:  \(manager.totalScore.description)"
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
